356b87d136f00434be8d42790af8447672da1485
IL1ne <IL1ne@yandex.ru>
2020-02-21 01:18:29 +0300

    feat: resolve task and passing all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  
+    return String(n).match(/[0-9]/g).reverse().join('') //Если регулярное выражение содержит флаг g, метод вернёт массив, содержащий все сопоставления. 
 }
